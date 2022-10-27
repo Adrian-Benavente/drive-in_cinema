7d390bf809efbf7488f1e296b728d5b672430777
Adrián Benavente <benaventeadrian@gmail.com>
2022-10-27 16:29:58 -0300

    Increase texture pattern repetition

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -2,16 +2,16 @@ import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
 
 export const Ground = () => {
-  const groundTexture = useTexture("/grass.jpg");
-  groundTexture.repeat = new THREE.Vector2(3, 3);
-  groundTexture.wrapS = THREE.RepeatWrapping;
-  groundTexture.wrapT = THREE.RepeatWrapping;
+  const groundTexture = useTexture({ map: "/grass.jpg" });
+  groundTexture.map.repeat = new THREE.Vector2(8, 8);
+  groundTexture.map.wrapS = THREE.RepeatWrapping;
+  groundTexture.map.wrapT = THREE.RepeatWrapping;
 
   return (
     <>
       <mesh position={[0, 1.52, 0]} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
         <planeGeometry args={[40, 40]} />
-        <meshStandardMaterial map={groundTexture} />
+        <meshStandardMaterial {...groundTexture} />
       </mesh>
     </>
   );

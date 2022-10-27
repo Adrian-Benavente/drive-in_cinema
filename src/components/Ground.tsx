ca71c1c4ed8859c6a6b4d28a02e7554aba203e1d
Adrián Benavente <benaventeadrian@gmail.com>
2022-10-27 11:36:01 -0300

    Set textures to repeat (still doesn't work)

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -2,13 +2,15 @@ import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
 
 export const Ground = () => {
-  const groundTexture = useTexture({ map: "/grass.jpg" });
+  const groundTexture = useTexture("/grass.jpg");
+  groundTexture.wrapS = THREE.RepeatWrapping;
+  groundTexture.wrapT = THREE.RepeatWrapping;
 
   return (
     <>
       <mesh position={[0, 1.52, 0]} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
         <planeGeometry args={[40, 40]} />
-        <meshStandardMaterial {...groundTexture} />
+        <meshStandardMaterial map={groundTexture} />
       </mesh>
     </>
   );
